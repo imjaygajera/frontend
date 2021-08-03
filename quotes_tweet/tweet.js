00d2a32d4ba2a47cbfeb5e29bb08e2f8b6de93b9
Jay-Bravo <jay.g@solutionfounder.com>
2021-08-03 16:30:06 +0530

    [IMP] js

--- quotes_tweet/tweet.js
+++ quotes_tweet/tweet.js
@@ -1,19 +1,27 @@
 "use strict";
-const _ = {
-const quotes = document.getElementById("quotes");
-        console.log(quotes)
+	const quotes = document.getElementById("quotes");
         const author = document.getElementById("author");
         const new_Q = document.getElementById("new_Q");
         const tweetMe = document.getElementById("tweetMe");
         let realData = "";
         let quotesData = "";
+        let realImg = "";
+        const J = {
+	W:window,
+	D:document,
+	H:history,
+	L:localStorage,
+	chrome:navigator.userAgent.indexOf("Chrome"),
+	S:['addEventListener','load','resize','keyup','click','change'],
+	gret:"",
+	BGImage:""
+}
         const tweetNow = () => {
             let  tweetPost = `https://twitter.com/compose/tweet?text=${quotesData.text} - ${quotesData.author}`;
             window.open(tweetPost)
         };
         const getNewQuotes = () => {
-            let rnum = Math.floor(Math.random() * 101);
-            console.log(rnum)
+            let rnum = Math.floor(Math.random()*Math.floor(realData.length));
             quotesData = realData[rnum]
             quotes.innerText = `${quotesData.text}`;
             quotesData.author == null   
@@ -22,13 +30,18 @@ const quotes = document.getElementById("quotes");
         };
         const getQuotes = async () => {
             const api = "https://type.fit/api/quotes";
+            const imgApi = 'https://picsum.photos/'+J.W.innerWidth+'/'+J.W.innerHeight;
             try {
                 let data = await fetch(api);
                 realData = await data.json();
                 getNewQuotes();
-                console.log(realData);
             } catch (error) {}
-            
+            try{
+                let img = await fetch(imgApi)
+                let ImgAPi = document.getElementById("ImgAPi");
+                ImgAPi.src = img.url
+                realImg = await img.json();
+            }catch (error) {}
         };
         tweetMe.addEventListener("click",tweetNow);
         new_Q.addEventListener("click",getNewQuotes);
